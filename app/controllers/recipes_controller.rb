753fefd9ffb99e04d0ab0f27200a6de258f858fd
Mary Jack <maryjackinthebox@gmail.com>
2018-10-12 00:35:43 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,5 +14,19 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
+  end
+  
+   private
+   
+   def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :quantity,
+        :name
+      ]
+    )
   end
 end
